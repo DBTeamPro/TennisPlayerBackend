f8c952e415ddd401ee33b7a0f81a4448655ad202
SwarzChen <aaaa102234@gmail.com>
2021-04-22 13:57:49 +0800

    [main] remove unused files

--- user_api/create_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE DimDate2 (
-        DateId INT NOT NULL,
-        CalendarQuarter TINYINT NOT NULL,
-        FiscalQuarter TINYINT NOT NULL
-);
--- user_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from user_api import views
-
-urlpatterns = [
-    path('test', views.test),
-]
