d01d152c272c8424877e0dc9ae7668f64bfe58b3
SwarzChen <aaaa102234@gmail.com>
2021-05-16 00:49:10 +0800

    [main] activate match api

--- match_api/urls.py
+++ match_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from match_api import views
+from match_api import controllers
 
 urlpatterns = [
-    path('rank', views.get_rank_analysis),
-    path('seed', views.get_seed_analysis),
+    path('rank', controllers.get_rank_analysis),
+    path('seed', controllers.get_seed_analysis),
+    path('goal', controllers.get_goal_analysis),
+    path('underdog', controllers.get_underdog_analysis),
+    path('firstplace', controllers.get_firstplace_analysis),
 ]
