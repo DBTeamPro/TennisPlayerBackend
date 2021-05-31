ed208c65bf418bbaea3580e50c5fdf2befb94c5b
SwarzChen <aaaa102234@gmail.com>
2021-05-31 17:01:41 +0800

    [main] activate new url function

--- TennisBackend/urls.py
+++ TennisBackend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('init/', include('load_api.urls')),
     path('match/', include('match_api.urls')),
     path('player/', include('player_api.urls')),
+    path('crud/', include ('CRUD_api.urls')),
 ]
