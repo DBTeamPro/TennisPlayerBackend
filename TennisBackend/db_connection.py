8de45271a1d69b62b4e5e54cf14974d1d0b0333a
SwarzChen <aaaa102234@gmail.com>
2021-06-02 17:38:46 +0800

    [main] new execute function

--- TennisBackend/db_connection.py
+++ TennisBackend/db_connection.py
@@ -56,6 +56,35 @@ def azure_parse_gotten_data(command_line):
                 data.append(temp)
             return data
 
+def azure_parse_small_matches(command_line):
+    with pyodbc.connect(
+                'DRIVER=' + config["driver"] + \
+                ';SERVER='+config["server"] + \
+                ';PORT=1433;DATABASE=' + config["database"] + \
+                ';UID='+config["username"] + 
+                ';PWD='+ config["password"]
+                ) as conn:
+        with conn.cursor() as cursor:
+            rows = cursor.execute(command_line).fetchall()
+            data = []
+            for row in rows:
+                temp = {}
+                temp["id"] = row.id
+                temp["surface"] = row.surface
+                temp["winner_name"] = row.winner_name
+                temp["winner_hand"] = row.winner_hand
+                temp["winner_ht"] = row.winner_ht
+                temp["winner_age"] = row.winner_age
+                temp["loser_name"] = row.loser_name
+                temp["loser_hand"] = row.loser_hand
+                temp["loser_ht"] = row.loser_ht
+                temp["loser_age"] = row.loser_age
+                temp["score"] = row.score
+                temp["minutes"] = row.minutes
+
+                data.append(temp)
+            return data
+
 
 
 
