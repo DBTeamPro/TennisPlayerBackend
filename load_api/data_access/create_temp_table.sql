0bf9e0b33470a21b91c31561291211dfa0b7c3a6
SwarzChen <aaaa102234@gmail.com>
2021-06-02 17:38:22 +0800

    [main] new table structure

---
+++ load_api/data_access/create_temp_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE MyMatch (
+    id int,
+	surface varchar(300) ,
+	winner_name varchar(300) ,
+	winner_hand varchar(300) ,
+	winner_ht int ,
+	winner_age float ,
+	loser_name varchar(300) ,
+	loser_hand varchar(300) ,
+	loser_ht int ,
+	loser_age float  ,
+	score varchar(300) ,
+	minutes int ,
+);
