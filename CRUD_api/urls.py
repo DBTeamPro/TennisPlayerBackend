90a5427bfab2d3b0ea0045c7a873534e2cceac02
SwarzChen <aaaa102234@gmail.com>
2021-06-02 17:37:52 +0800

    [main] activate all CRUD apis

--- CRUD_api/urls.py
+++ CRUD_api/urls.py
@@ -5,4 +5,8 @@ from CRUD_api import controllers
 urlpatterns = [
     path('search_match', controllers.search_match),
     path('search_player', controllers.search_player),
+    path('create', controllers.create_match),
+    path('small_matches', controllers.get_all_my_match),
+    path('update/<slug:id>', controllers.update_match),
+    path('delete/<slug:id>', controllers.delete_match),
 ]
